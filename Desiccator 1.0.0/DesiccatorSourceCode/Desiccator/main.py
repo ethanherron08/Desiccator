c7a8fd2adc40b55db169b3d257cd023358bc20e5
ethanherron08 <37049556+ethanherron08@users.noreply.github.com>
2026-07-30 09:10:21 -0400

    Desiccator 1.0.0

---
+++ Desiccator 1.0.0/DesiccatorSourceCode/Desiccator/main.py
@@ -0,0 +1,22 @@
+# main.py
+
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from gui import ScaleLoggerGUI
+
+
+def main():
+
+    app = QApplication(sys.argv)
+
+    window = ScaleLoggerGUI()
+
+    window.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
